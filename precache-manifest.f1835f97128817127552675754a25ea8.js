ded636c824fa2a5d65c44d70ae17075701ce8fbf
ashwin <ashwinshan95@gmail.com>
2019-11-14 20:04:48 +0530

    Updates

---
+++ precache-manifest.f1835f97128817127552675754a25ea8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a2e202cccc495d2515f681b5bb2701c",
+    "url": "/fairbeat/index.html"
+  },
+  {
+    "revision": "d87a557cbb943a560d8b",
+    "url": "/fairbeat/static/css/2.953edc3d.chunk.css"
+  },
+  {
+    "revision": "26f3860833d052d97ef6",
+    "url": "/fairbeat/static/css/main.1ef1c624.chunk.css"
+  },
+  {
+    "revision": "d87a557cbb943a560d8b",
+    "url": "/fairbeat/static/js/2.bb1407f6.chunk.js"
+  },
+  {
+    "revision": "26f3860833d052d97ef6",
+    "url": "/fairbeat/static/js/main.16b0a01d.chunk.js"
+  },
+  {
+    "revision": "4a7f270379348a6a7e25",
+    "url": "/fairbeat/static/js/runtime-main.06920252.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fairbeat/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/fairbeat/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fairbeat/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fairbeat/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fairbeat/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b4a0edd0089c4414409af779f89307ec",
+    "url": "/fairbeat/static/media/picture.b4a0edd0.png"
+  }
+]);
